62e5418b3cb969ba78f5d4ade3c2ec9103ee00c2
Lauren Kleinert <lauren.o.kleinert@gmail.com>
2022-06-11 12:09:18 -0400

    Completed waves 2, 3; made modifications to HTML/CSS files as needed

--- src/index.js
+++ src/index.js
@@ -1,54 +1,65 @@
 'use strict';
 
 const state = {
-  temperature: 75,
-  tempColor: 'black',
-  landscape: "url('photos/spring.jpeg')",
+  temp: 75,
+  tempColor: 'warm',
+  landscape: 'body_warm',
+  currentCity: 'Phoenix',
 };
 
 const tempColor = () => {
   const tempValue = document.getElementById('temp-value');
   const body = document.getElementById('body');
-  if (state.temperature >= 80) {
-    state.tempColor = 'red';
-    tempValue.style.color = state.tempColor;
-    state.landscape = "url('photos/best_desert.jpeg')";
-    body.style.backgroundImage = state.landscape;
-  } else if (state.temperature >= 70 && state.temperature <= 79) {
-    state.tempColor = 'orange';
-    tempValue.style.color = state.tempColor;
-    state.landscape = "url('photos/spring.jpeg')";
-    body.style.backgroundImage = state.landscape;
-  } else if (state.temperature >= 60 && state.temperature <= 69) {
-    state.tempColor = 'yellow';
-    tempValue.style.color = state.tempColor;
-    state.landscape = "url('photos/fall_4.jpeg')";
-    body.style.backgroundImage = state.landscape;
-  } else if (state.temperature >= 50 && state.temperature <= 59) {
-    state.tempColor = 'green';
-    tempValue.style.color = state.tempColor;
-    state.landscape = "url('photos/fall_3.jpeg')";
-    body.style.backgroundImage = state.landscape;
-  } else if (state.temperature <= 49) {
-    state.tempColor = 'teal';
-    tempValue.style.color = state.tempColor;
-    state.landscape = "url('photos/colorado-snow.jpeg')";
-    body.style.backgroundImage = state.landscape;
+  if (state.temp >= 80) {
+    state.tempColor = 'hot';
+    tempValue.className = state.tempColor;
+    state.landscape = 'body_hot';
+    body.className = state.landscape;
+  } else if (state.temp >= 70 && state.temp <= 79) {
+    state.tempColor = 'warm';
+    tempValue.className = state.tempColor;
+    state.landscape = 'body_warm';
+    body.className = state.landscape;
+  } else if (state.temp >= 60 && state.temp <= 69) {
+    state.tempColor = 'cool';
+    tempValue.className = state.tempColor;
+    state.landscape = 'body_cool';
+    body.className = state.landscape;
+  } else if (state.temp >= 50 && state.temp <= 59) {
+    state.tempColor = 'chilly';
+    tempValue.className = state.tempColor;
+    state.landscape = 'body_chilly';
+    body.className = state.landscape;
+  } else if (state.temp <= 49) {
+    state.tempColor = 'cold';
+    tempValue.className = state.tempColor;
+    state.landscape = 'body_cold';
+    body.className = state.landscape;
   }
 };
 
-const increaseTemperature = () => {
-  state.temperature += 1;
+const increaseTemp = () => {
+  state.temp += 1;
   const tempValue = document.getElementById('temp-value');
-  tempValue.textContent = `${state.temperature}`;
+  tempValue.textContent = `${state.temp}`;
   tempColor();
 };
 
-const decreaseTemperature = () => {
-  state.temperature -= 1;
+const decreaseTemp = () => {
+  state.temp -= 1;
   const tempValue = document.getElementById('temp-value');
-  tempValue.textContent = `${state.temperature}`;
+  tempValue.textContent = `${state.temp}`;
   tempColor();
+};
+
+const changeCity = () => {
+  console.log(state.currentCity);
+  const newCityName = document.getElementById('enter-city-name');
+  console.log(newCityName.value);
+  state.currentCity = newCityName.value;
+  console.log(state.currentCity);
+  const defaultCity = document.getElementById('default-city');
+  defaultCity.textContent = state.currentCity;
 };
 
 // const updateDogContainer = () => {
@@ -59,10 +70,13 @@ const decreaseTemperature = () => {
 
 const registerEventHandlers = (event) => {
   const upArrow = document.getElementById('up-arrow');
-  upArrow.addEventListener('click', increaseTemperature);
+  upArrow.addEventListener('click', increaseTemp);
 
   const downArrow = document.getElementById('down-arrow');
-  downArrow.addEventListener('click', decreaseTemperature);
+  downArrow.addEventListener('click', decreaseTemp);
+
+  const newCityName = document.getElementById('enter-city-name');
+  newCityName.addEventListener('input', changeCity);
 };
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
